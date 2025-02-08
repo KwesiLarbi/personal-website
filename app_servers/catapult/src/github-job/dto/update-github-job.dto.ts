139fe044ebf56d8836dd011efbc1a6dc677d552d
KwesiLarbi <KwesiiiLarb@gmail.com>
2025-02-08 09:32:32 -0500

    updating code structure

--- app_servers/catapult/src/github-job/dto/update-github-job.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateGithubJobDto } from './create-github-job.dto';
-
-export class UpdateGithubJobDto extends PartialType(CreateGithubJobDto) {}
